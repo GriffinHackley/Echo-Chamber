ce2214449ec3f5a5fa10de4c12dc0809c9ed6996
Griffin Hackley <griffinhackley@yahoo.com>
2021-06-09 19:06:33 -0600

    Finished part 2. Final Commit

--- voting.py
+++ voting.py
@@ -61,7 +61,7 @@ def partOne(voters, candidates, tup):
     win = winner(voters, ordered, candidates, names, [])
     print("The winner is", win)
     ordered = deepcopy(tup[0])
-    socialWelfare(win, ordered, candidates, names, candidateRanking)
+    socialWelfare(win, ordered, candidateRanking)
     
 def winner(voters, rankings, candidates, names, removed):
     rankings = deepcopy(rankings)
@@ -87,7 +87,6 @@ def winner(voters, rankings, candidates, names, removed):
         if not (i in removed):
             if count[i] < count[min]:
                 min = i
-    # print(count)
 
     #check if there is a tie for least wins
     tie = False
@@ -120,7 +119,7 @@ def winner(voters, rankings, candidates, names, removed):
 
         min = indices[min]
 
-    #remove candidate with least first choices
+    #remove candidate
     for i in range(voters):
         rankings[i].remove(min)
     removed.append(min)
@@ -134,11 +133,9 @@ def printOrdered(names, ordered, voters):
         print(names[i], end=" ")
         print(ordered[i])
 
-def socialWelfare(win, ordered, candidates, names, candidateRanking):
+def socialWelfare(win, ordered, candidateRanking):
     ordinal = []
     cardinal = []
-
-    # print(candidateRanking)
 
     for i in range(voters):
         #get where the winner ranked in this persons order
@@ -149,95 +146,130 @@ def socialWelfare(win, ordered, candidates, names, candidateRanking):
         pref = round(-pow(candidateRanking[i][ordered[i][0]-1][1] - candidateRanking[i][win-1][1],2),2)
         cardinal.append(pref)
 
-    print("Ordinal Social Welfare:",sum(ordinal))
-    print("Cardinal Social Welfare:",sum(cardinal))
-    print("")
+    # print("Ordinal Social Welfare:",sum(ordinal))
+    # print("Cardinal Social Welfare:",sum(cardinal))
+    # print("")
+    return (sum(ordinal), sum(cardinal))
 
 def partTwo(voters, candidates, tup):
     ordered = deepcopy(tup[0])
     names = tup[1]
+    rankings = tup[2]
     connections = deepcopy(tup[3])
+
+    #get initial first choices
+    votes = []
+    for x in ordered:
+        votes.append(x[0])
 
     print_connections(names, connections, voters, candidates)
     print("")
 
     print("-----REAL RESULTS-----")
-    print("The winner of the real election is:", winner(voters, ordered, candidates, names, []))
+    orig = getWinner(votes.copy())
+    print("The winner of the real election is:", orig)
+    origWelfare = 0
+    for wins in orig:
+        origWelfare = origWelfare + socialWelfare(wins, deepcopy(tup[0]), rankings)[0]
+    origWelfare = origWelfare/len(orig)
+    
 
     print("")
     print("-----DISHONEST RESULTS-----")
 
     printOrdered(names, ordered, voters)
+    print()
 
-    tup = singleElimination(voters, ordered, candidates, names, [])
-    ordered = tup[0]
-    removed = tup[1]
-    candidates = candidates - 1 
+    changedMinds = -1
+    while not changedMinds == 0:
+        me = 0
+        lastMinds = changedMinds
+        changedMinds = 0
+        for voter in connections:
+            #get decision for this round
+            tuple = getDecision(votes, [names[me], ordered[me].copy()], orig, votes[me])
+            changedMinds = changedMinds + tuple[1]
+            if tuple[1] == 1:
+                print(names[me], "changed their mind from", votes[me], "to", tuple[0])
+            votes[me] = tuple[0]
+            me = me+1
+        print("Number of Minds Changed:", changedMinds)
+        print("Round ended")
+        print()
+        if changedMinds <= lastMinds-1:
+            break
+        win = getWinner(votes)
+    
+    win = getWinner(votes)
+    print("The dishonest winners are:", win)
 
-    #generate model of echo chamber
-    me = 0
-    votes = []
-    changedMinds = 0
-    for voter in connections:
-        echoName = []
-        echoVote = []
-        count = 0
-        for i in voter:
-            if i == 1:
-                echoName.append(names[count])
-                echoVote.append(ordered[count].copy())
-            count = count+1
-        tup = getDecision(echoName, echoVote, candidates, [names[me],ordered[me].copy()])
-        if tup[1]:
-            changedMinds = changedMinds + 1
-        votes.append(tup[0])
-        me = me+1
+    welfare = 0
+    for wins in win:
+        welfare = welfare + socialWelfare(wins, deepcopy(tup[0]), rankings)[0]
+    welfare = welfare/len(win)
 
-    for vote in votes:
-        print(vote)
+    print("The social welfare of the original winner is", origWelfare)
+    print("The social welfare of the dishonest vote is", welfare)
 
-def getDecision(names, ordered, candidates, me):
-    current = deepcopy(ordered)
-    names.append(me[0])
-    current.append(me[1].copy())
-    # for x in range(len(names)):
-    #     print(names[x],":",current[x])
-    orig = winner(len(names), current, candidates, names, [])
-    best = me[1]
-    changedMind = False    
+def getDecision(votes, me, orig, myVote):
+    #get winner
+    orig = getWinner(votes.copy())
+    temp = 0
+    for x in orig:
+        temp = temp + welfare(x, me[1])
+    bestWel = temp/len(orig)
 
-    #try permutations where any candidate ranked higher than the winner is put in first place
-    if not orig == me[1][0]:
-        # print("Original Winner:", orig)
-        # print("Original Welfare:", welfare(orig, me[1]))
-        for i in range (1,candidates):
-            others = deepcopy(ordered)
+    #if prefered candidate will win anyways, just vote for them
+    if myVote in orig:
+        return (myVote,0)
 
-            # add new permutation
-            test = me[1].copy()
-            if test[i] == orig:
-                break
-            test.insert(0, test.pop(i))
+    
+    bestVote = myVote
+    for i in range(1,len(me[1])-1):
+        # find winners if this person votes for every different person 
+        # (except their first and last choices)
+        temp = votes.copy()
+        temp.append(me[1][i])
+        winners = getWinner(temp)
 
-            others.append(test)
+        #check if the winners change
+        if not winners == orig:
+            #get welfare of new winners
+            temp = 0
+            for x in winners:
+                temp = temp + welfare(x, me[1])
+            newWel = temp/len(winners)
+            
 
-            #find welfare of this permutation
-            current = winner(len(names), others, candidates, names, [])
-            others = deepcopy(ordered)
-            others.append(best)
-            toBeat = winner(len(names), others, candidates, names, [])
-            if not current == orig:
-                if welfare(current, me[1]) > welfare(toBeat, me[1]):
-                    best = test
-                    changedMind = True
-                    # print("HERE-----------------------------------------")
-                    # print(test)
-                    # print("New Winner:", current)
-                    # print("New Welfare:", welfare(current, me[1]))
-    # else:
-    #     print("It matched")
-    # print("")
-    return (best,changedMind)
+            #check if new welfare is better than old welfare
+            if newWel > bestWel:
+                bestVote = me[1][i]
+                bestWel = newWel
+    
+    if bestVote == myVote:
+        return bestVote,0
+    else:
+        return bestVote,1
+
+def getWinner(votes):
+    #count number of votes
+    count = [0 for x in range(candidates+1)] 
+    for i in votes:
+        count[i-1] = count[i-1]+1
+    
+    #find winner
+    max = 0
+    counter = 0
+    winners = []
+    for x in count:
+        if x > max:
+            max = x
+            winners = []
+            winners.append(counter+1)
+        elif x == max:
+            winners.append(counter+1)
+        counter = counter + 1
+    return winners
 
 def singleElimination(voters, rankings, candidates, names, removed):
     rankings = deepcopy(rankings)
@@ -307,8 +339,8 @@ def welfare(winner, order):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    voters = 7
-    candidates = 4
+    voters = 5
+    candidates = 3
     tup = create_voting(voters, candidates, False)
     print("")
     # partOne(voters, candidates, tup)
